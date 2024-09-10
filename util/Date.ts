d9f917a01cb48ccb80797054fc5da725994a041f
Gagan choudhary <55543086+Gaganchoudhary12@users.noreply.github.com>
2024-09-10 15:13:38 +0530

    Delete util directory

--- util/Date.ts
+++
@@ -1,7 +0,0 @@
-import { DateValue } from "@nextui-org/react";
-import moment from "moment";
-
-export const getFormateDateOfINtDate = (date:DateValue) => {
-    return moment(`${date.month}/${date.day}/${date.year}`).toDate()
-    
-}
