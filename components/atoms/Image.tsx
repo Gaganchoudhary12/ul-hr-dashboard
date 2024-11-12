cbc2430adb876c2594f1b2c1d990b14f50be41e4
Gagan choudhary <55543086+Gaganchoudhary12@users.noreply.github.com>
2024-11-12 10:41:53 +0530

    Delete components directory

--- components/atoms/Image.tsx
+++
@@ -1,7 +0,0 @@
-import NextImage, { ImageProps } from  "next/image";
-
-const Image = (props:ImageProps) => 
-    <NextImage {...props} />
-
-
-export default Image
--- components/atoms/Loader.tsx
+++
@@ -1,5 +0,0 @@
-import { Spinner } from "@nextui-org/react";
-
-const Loader = () => <Spinner size="lg" />;
-
-export default Loader;
