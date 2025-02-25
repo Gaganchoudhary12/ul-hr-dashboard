8511755498ab537a9d6f4ddbeffb0783311ce305
Gagan choudhary <55543086+Gaganchoudhary12@users.noreply.github.com>
2025-02-25 16:47:27 +0530

    Update route.ts

--- app/api/ideas/route.ts
+++ app/api/ideas/route.ts
@@ -3,9 +3,10 @@ import mongoose from "mongoose";
 const ObjectId = mongoose.Types.ObjectId;
 import Ideas from "../../../lib/models/ideas";
 
-export async function GET(): Promise<NextResponse> {
+export async function GET(req: any,res: any): Promise<NextResponse> {
   try {
     await mongoose.connect(process.env.DB_URL as string);
+      res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
     const ideas = await Ideas.find({}).sort({ date: -1 }).lean();
     return NextResponse.json(
       { data: ideas },
