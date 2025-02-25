1c8d23a36790cb9ab3c88bda8958321b8b4018b2
Gagan choudhary <55543086+Gaganchoudhary12@users.noreply.github.com>
2025-02-25 17:06:56 +0530

    Update route.ts

--- app/api/ideas/route.ts
+++ app/api/ideas/route.ts
@@ -5,8 +5,8 @@ import Ideas from "../../../lib/models/ideas";
 
 export async function GET(req: any): Promise<NextResponse> {
   try {
-     const url = new URL(req.url);
-    const _id = new URLSearchParams(url.searchParams).get("id");
+    //  const url = new URL(req.url);
+    // const _id = new URLSearchParams(url.searchParams).get("id");
     await mongoose.connect(process.env.DB_URL as string);
     const ideas = await Ideas.find({}).sort({ date: -1 }).lean();
     return NextResponse.json(
