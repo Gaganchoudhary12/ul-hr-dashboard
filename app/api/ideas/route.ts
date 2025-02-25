0f5c6d796a4d6de600f8e8a5967de11928ac7ca8
Gagan choudhary <55543086+Gaganchoudhary12@users.noreply.github.com>
2025-02-25 16:52:56 +0530

    Update route.ts

--- app/api/ideas/route.ts
+++ app/api/ideas/route.ts
@@ -6,12 +6,17 @@ import Ideas from "../../../lib/models/ideas";
 export async function GET(req: any,res: any): Promise<NextResponse> {
   try {
     await mongoose.connect(process.env.DB_URL as string);
-      res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
     const ideas = await Ideas.find({}).sort({ date: -1 }).lean();
     return NextResponse.json(
       { data: ideas },
       {
         status: 200,
+         headers: {
+            "Content-Type": "application/json",
+            "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
+            "Pragma": "no-cache",
+            "Expires": "0",
+          },
       }
     );
   } catch (error) {
